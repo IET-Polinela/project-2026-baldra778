6aafcfc6db8c4805a2fa8ac8327535604dc6ee3b
BagasArya <bagasaja633@gmail.com>
2026-06-22 23:31:12 +0700

    labsession14 add skenario1

--- server_smartcity/usermanagement_24782007/api_views.py
+++ server_smartcity/usermanagement_24782007/api_views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
+from drf_spectacular.utils import extend_schema
 from .serializers import RegisterSerializer
 from .models import User
 
-
+@extend_schema(exclude=True)
 class RegisterView(generics.CreateAPIView):
 
     queryset = User.objects.all()
